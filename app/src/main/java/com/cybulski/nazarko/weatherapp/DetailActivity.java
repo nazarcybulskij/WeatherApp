35ad20931525da522c9edde9fadac79efa149318
Nazarko Zipolino <Nazarko Zipolino>
2016-01-26 14:06:17 +0200

    added  kml layer to map

--- app/src/main/java/com/cybulski/nazarko/weatherapp/DetailActivity.java
+++ app/src/main/java/com/cybulski/nazarko/weatherapp/DetailActivity.java
@@ -10,15 +10,24 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.cybulski.nazarko.weatherapp.model.WheaterEvent;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.maps.android.kml.KmlLayer;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
 
 import io.realm.Realm;
 
 public class DetailActivity extends AppCompatActivity {
-    private Realm realm;
 
+    private Realm realm;
     TextView  tvId;
     TextView tvTitle;
     TextView tvArea;
+    SupportMapFragment mapFragment;
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +46,25 @@ public class DetailActivity extends AppCompatActivity {
             tvTitle.setText(wheaterEvent.getTitle());
             tvArea.setText(wheaterEvent.getAreaDesc());
         }
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        map = mapFragment.getMap();
+        if (map == null) {
+            finish();
+            return;
+        }else{
+            try {
+                KmlLayer layer = new KmlLayer(map, R.raw.cb_2014_us_county_20m, getApplicationContext());
+                layer.addLayerToMap();
+            } catch (XmlPullParserException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
 
 
 
